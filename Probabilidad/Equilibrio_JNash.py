53399138c4dc028b85a9a6c5ae6242949be1333a
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-01-18 09:18:28 -0600

    Update Equilibrio_JNash.py

--- Probabilidad/Equilibrio_JNash.py
+++ Probabilidad/Equilibrio_JNash.py
@@ -1,1 +1,5 @@
 
+http://relopezbriega.github.io/blog/2015/06/27/probabilidad-y-estadistica-con-python/
+https://www.google.com.mx/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8&client=ubuntu#q=probabilidad%20python
+http://relopezbriega.github.io/blog/2016/11/26/introduccion-a-la-teoria-de-probabilidad-con-python/
+http://pybonacci.org/2012/04/21/estadistica-en-python-con-scipy/
