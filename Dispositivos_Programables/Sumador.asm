97a869e1ee305e621a0f1d85ede1dbcbd25d452b
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-01-19 22:17:32 -0600

    Update Sumador.asm

--- Dispositivos_Programables/Sumador.asm
+++ Dispositivos_Programables/Sumador.asm
@@ -1,1 +1,21 @@
 
+__CONFIG _CP_OFF & _WDT_OFF & _PWRTE_ON & _XT_OSC
+LIST P=16F84A
+INCLUDE <P16F84A.INC>
+
+K_Const EQU 0x0F ;
+V_Dato EQU 0x0C ;
+
+ORG 0x00
+Inicio bsf STATUS,RP0 ;
+clrf TRISB ;
+movlw 0xFF ;
+movwf TRISA ;
+bcf STATUS,RP0 ;
+
+Main movf PORTA,W ;
+addlw K_Const ;
+movwf V_Dato ;
+movwf PORTB ;
+goto Main ;
+END
