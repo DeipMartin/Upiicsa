d3705d36b1eb02ca5736758b8248fded9db535a1
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-15 00:14:43 -0500

    Update Listas.pl

--- Fundamentos_IA/Listas.pl
+++ Fundamentos_IA/Listas.pl
@@ -1,1 +1,9 @@
+alumno(X,[X|_]).
+alumno(X,[_|Y]) :- alumno(X,Y).
 
+elementos_en_lista([],0).
+elementos_en_lista([X|Y],N) :-elementos_en_lista(Y,W), N is W+1.
+
+/* Concatenamos las listas Y1 y Y2 dando origen a la lista Y3 */
+concatena([],Y,Y).
+concatena([X|Y1],Y2,[X|Y3]) :- concatena(Y1,Y2,Y3).
