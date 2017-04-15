cf91711f0872b13733bebea341bb8447a3c1e2e8
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-15 00:06:26 -0500

    Update ArbolFamilia.pl

--- Fundamentos_IA/ArbolFamilia.pl
+++ Fundamentos_IA/ArbolFamilia.pl
@@ -1,1 +1,17 @@
 
+mujer(alejandra).
+mujer(ana).
+
+hombre(roberto).
+hombre(marco).
+hombre(juan).
+
+padre_de(roberto,ana).
+padre_de(roberto,marco).
+
+madre_de(alejandra,ana).
+madre_de(alejandra,marco).
+
+abuelo_de(juan,ana).
+abuelo_de(juan,marco).
+padre_de(juan,roberto).
