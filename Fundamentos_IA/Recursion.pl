ee19dbe530c38f54e39ebedd7f46b8d3a6e925ac
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-05-07 22:38:46 -0500

    Update Recursion.pl

--- Fundamentos_IA/Recursion.pl
+++ Fundamentos_IA/Recursion.pl
@@ -1,1 +1,5 @@
-
+Call: ( 6) nel([a,[b,c],d], _G309) ? creep
+Call: ( 7) nel([[b,c],d], _L104) ? creep
+Call: ( 8) nel([d], _L117) ? creep
+Call: ( 9) nel([], _L130) ? creep
+Exit: ( 9) nel([], 0) ? creep
