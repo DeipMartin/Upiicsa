9ec4099111d4ed546fa75c3abbde8b7742ac428b
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-05-27 23:03:30 -0500

    Update Joins1.sql

--- Base_Datos/Joins1.sql
+++ Base_Datos/Joins1.sql
@@ -1,1 +1,6 @@
+SELECT ORDER.ORDERID AS ID , CUSTOMERS.CUSTOMERSNAME AS [CUSTOM NAME] , SHIPPERS.
 
+SELECT Orders.OrderID as ID , Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
